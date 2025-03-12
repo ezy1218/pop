744dac1a014db6c8c9855bb11421a2c02691b94f
ezy1218 <ziyoda110@icloud.com>
2025-03-12 22:44:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// console.log('ismingizni kiriting')
+alert('malumotingizni kiritganiz uchun tashakur')
+
+// var box ="nozi boq"
+// console.log(box)
+
+// let age = 25;
+// age = 30;
+// console.log(age);
+
+// const pi = 3.14;
+
+
+let name1 = prompt('ismingizni kiriting')
+console.log(name1)
+
+let familiye = prompt('famlyangizni kiriting')
+console.log(familiye)
+
+let vozrast = prompt('yoshingizni kiriting')
+console.log(vozrast)
+
+let fan = prompt('yoqtirgan faningizni yozing')
+console.log(fan)
+
+let tuman = prompt('yashayatgan tumaningizni yozing')
+console.log(tuman)
+
+alert('malumotingizni kiritganiz uchun tashakur')
+
+
+
+
+
